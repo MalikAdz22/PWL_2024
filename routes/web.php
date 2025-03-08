8941180adb4a1736dc7797014c27d90fa7e4ad5b
Malik Adzano Aryasatya Dharmaputera <adzanomalik@gmail.com>
2025-03-08 11:09:04 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Profiler\Profile;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+Route::get('/hello world', function () { 
+    return 'hello world'; 
 });
+
+Route::get('/world', function () { 
+    return 'world'; 
+});
+
+Route::get('/', function () { 
+    return 'selamat datang'; 
+});
+
+Route::get('/about', function () { 
+    return 'Malik Adzano - 2341760161'; 
+});   
+
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya '.$name; 
+});
+
+Route::get('/posts/{post}/comments/{comment}', function  ($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) { 
+    return 'Halaman artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name; 
+});
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name; 
+});
+*/
+
+Route::get('/user/profile', function () { 
+    return "user/profile";
+   })->name('profile'); 
+
