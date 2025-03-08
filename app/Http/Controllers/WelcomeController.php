69d94cac144faea1995fc78548bf5d6781551366
Malik Adzano Aryasatya Dharmaputera <adzanomalik@gmail.com>
2025-03-08 13:32:30 +0700

    Praktikum 3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,17 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
-{
-    public function index() { 
-        return 'Selamat Datang'; 
-       } 
-       
-    public function about() { 
-        return 'Malik Adzano : 2341760161'; 
-       }
-       
-    public function articles($id){
-        return 'Halaman artikel dengan id ' . $id;
-    }
-
+{ 
+    public function hello(){ 
+    return('Hello World'); 
+    } 
+   public function greeting(){ 
+    return view('blog.hello') 
+    ->with('name','Malik Adzano') 
+    ->with('occupation','football player'); 
+    } 
 }
+   
--- routes/web.php
+++ routes/web.php
@@ -82,4 +82,11 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy' 
 ]);
 
+/*
+Route::get('/greeting', function () { 
+    return view('blog.hello', ['name' => 'Malik Adzano Aryasatya Dharmaputera']); });
+*/
+
+Route::get('/greeting', [WelcomeController::class,  'greeting']);
+    
 
